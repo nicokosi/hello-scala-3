39d574671408cef0e839d8cbbdf9acec21322646
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2022-07-22 07:06:09 +0200

    Union types ⚖️
    https://docs.scala-lang.org/scala3/book/types-union.html

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,11 @@
 @main def hello(): Unit = println("Hello Scala 3! 👋")
+
+case class User(id: Int, name: String)
+case class Email(address: String)
+def help(id: User | Email): Unit =
+  id match
+    case User(_, name) => println(s"Need help, $name?")
+    case Email(address) => println(s"Support email has been sent to $address")
+@main def unionType(): Unit =
+  help(User(123, "John Doe"))
+  help(Email("JohnDoe@myprovider.com"))
